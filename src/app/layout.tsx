1e138c9f8fc84f9e40417b07cdf70fc3ffaaa49c
nikhil sharma <nikhil.sharma3755@gmail.com>
2025-12-18 12:48:04 +0530

    update layout.tsx(home)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ReduxProvider } from "@/providers/ReduxProvider";
 import ToastProvider from "@/components/ToastProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Foody",
@@ -31,9 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
           <ReduxProvider>
             <ToastProvider>{children}</ToastProvider>
           </ReduxProvider>
