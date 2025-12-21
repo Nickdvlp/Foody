d66c6b9c12a2453908a9c502332a1163c1335bde
nikhil sharma <nikhil.sharma3755@gmail.com>
2025-12-21 22:29:00 +0530

    route.ts(/api/notify)

--- src/app/api/notify/route.ts
+++ src/app/api/notify/route.ts
@@ -1,8 +1,11 @@
 import { realtime } from "@/lib/realtime";
 import { updateOrderStatus } from "@/modules/order/server/update-order-status";
 
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 export const POST = async (req: Request) => {
-  const body = await req.json(); // ✅ await here
+  const body = await req.json();
   const { orderId, newStatus } = body;
 
   await updateOrderStatus({ orderId, newStatus });
