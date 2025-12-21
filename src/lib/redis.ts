24b82bdac281ca68ce900f982f13963dffc35555
nikhil sharma <nikhil.sharma3755@gmail.com>
2025-12-21 22:39:14 +0530

    update redis.ts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,21 @@
 import { Redis } from "@upstash/redis";
 
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+let redis: Redis | null = null;
+
+export function getRealtime() {
+  if (!redis) {
+    if (
+      !process.env.UPSTASH_REDIS_REST_URL ||
+      !process.env.UPSTASH_REDIS_REST_TOKEN
+    ) {
+      throw new Error("Upstash env variables are missing");
+    }
+
+    redis = new Redis({
+      url: process.env.UPSTASH_REDIS_REST_URL,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    });
+  }
+
+  return redis;
+}
